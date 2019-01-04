dd67e65fc937959363bd44c58e3777b90b3235cc
joker <3123848646@qq.com>
2019-01-04 19:28:44 +0800

    2019-1-4

--- lib/demo/state/manage_statement_demo.dart
+++ lib/demo/state/manage_statement_demo.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 
-class ManageStatementDemo extends StatelessWidget {
+class ManageStatementDemo extends StatefulWidget {
+
+ @override
+ _ManageStatementDemoState createState() {
+    return _ManageStatementDemoState();
+  }
+}
+
+class _ManageStatementDemoState extends State<ManageStatementDemo>{
 
   int _count = 0;
+
+  void _increaseCount(){
+    setState(() {
+      _count += 1;
+      debugPrint("_count == $_count");
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,13 +26,40 @@ class ManageStatementDemo extends StatelessWidget {
         title: Text('ManageStatementDemo'),
         elevation: 0.0,
       ),
-      body: Center(
-        child: Chip(label: Text('$_count')),
-      ),
-      floatingActionButton: FloatingActionButton(child:Icon(Icons.add),onPressed: (){
-        _count += 1;
-        debugPrint("_count == $_count");
-      }),
+      body: CounterWrapper(_count,_increaseCount),
+      floatingActionButton: FloatingActionButton(child:Icon(Icons.add),onPressed:_increaseCount),
     );
   }
 }
+
+class Counter extends StatelessWidget {
+
+  final int count;
+
+  final VoidCallback increaseCallBack;
+
+  Counter(this.count,this.increaseCallBack);
+
+  @override
+  Widget build(BuildContext context) {
+    return ActionChip(label: Text('$count'),onPressed: increaseCallBack);
+  }
+}
+
+class CounterWrapper extends StatelessWidget {
+
+  final int count;
+
+  final VoidCallback increaseCallBack;
+
+  CounterWrapper(this.count,this.increaseCallBack);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Counter(count, increaseCallBack),
+    );
+  }
+}
+
+
